8bde09c0436cf61ccef2ed6ff06b9604beeb8264
Angel Chaile <angelchaile90@gmail.com>
2023-03-02 22:27:22 -0300

    agregando funciones a los botones

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
+const deleteChar = document.getElementById('borrar1');
+const deleteAll = document.getElementById('borraTodo');
 
 const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML))
 });
+
+deleteChar.addEventListener('click', () => display.borrar());
+deleteAll.addEventListener('click', () => display.borrarTodo());
