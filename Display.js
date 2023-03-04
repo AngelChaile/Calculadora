24667dec6c82b1f4ba6f5e411e12a07993e0187c
Angel Chaile <angelchaile90@gmail.com>
2023-03-03 22:37:12 -0300

    no funcionan los botones de operacion

--- Display.js
+++ Display.js
@@ -14,14 +14,22 @@ class Display {
     }
 
     borrarTodo(){
-        this.valorActual = ('');
-        this.valorAnterior = ('');
+        this.valorActual = '';
+        this.valorAnterior = '';
         this.tipoOperacion = undefined;
         this.imprimirValores();
     }
 
+    computar(tipo){
+        this.tipoOperacion !== "igual" && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
     agregarNumero(numero) {
-        if (numero === '.' && this.valorActual.includes('.')) return;
+        if ( numero === '.' && this.valorActual.includes('.') ) return;
         this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }
@@ -34,6 +42,9 @@ class Display {
     calcular(){
         const valorAnterior = parseFloat(this.ValorAnterior);
         const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual) || isNaN(valorAnterior) ) return;
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
     }
     
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
+
 const deleteChar = document.getElementById('borrar1');
 const deleteAll = document.getElementById('borraTodo');
 
@@ -13,3 +14,7 @@ botonesNumeros.forEach(boton => {
 
 deleteChar.addEventListener('click', () => display.borrar());
 deleteAll.addEventListener('click', () => display.borrarTodo());
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
